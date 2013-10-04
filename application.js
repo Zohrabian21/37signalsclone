db64ea049984e069f85298746383dea6830aa925
Nora Zohrabian <Zohrabian21@yahoo.com>
2013-10-04 14:28:23 -0400

    add application.js and update index and main files

--- application.js
+++ application.js
@@ -1,16 +1,4 @@
 $(document).ready(function() {
-<<<<<<< HEAD
-	$('.box1').hover(function() {
-      $('.h1').html("<span>Basecamp</span>is the project management tool you wish you had on your last project.");
-      $('.h2').html("Are you still managing projects with email? Are you still using Excel for your to-do lists? It's time to upgrade to Basecamp. Manage projects and collaborate with your team and clients the modern way."); 
-   });
-   $('.box1').hover(function() {
-   	  $('.h1')toggleClass('.h1box1');
-   	  $('.h2')toggleClass('.h2box2');
-   });
-   
-
-=======
 	$('.box:nth-child(1)').hover(function(){
 	  $('h1:nth-child(2), h2:nth-child(4)').hide();
 	  $('h1:nth-child(3), h2:nth-child(5), img:nth-child(6)').show();
@@ -42,5 +30,4 @@ $(document).ready(function() {
 	  $('h1:nth-child(2), h2:nth-child(4)').show();
 	  $('h1:nth-child(3), h2:nth-child(5), img:nth-child(6)').hide();
 	});
->>>>>>> master
 });
